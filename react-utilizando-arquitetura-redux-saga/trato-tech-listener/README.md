44acb53db39d3fb57dcd0afa90d499c727062154
eduqg <eduardoqgomes@gmail.com>
2023-11-24 11:17:16 -0300

    Finish course redux-saga

---
+++ react-utilizando-arquitetura-redux-saga/trato-tech-listener/README.md
@@ -0,0 +1,1 @@
+https://github.com/alura-cursos/trato-tech-saga
--- react-utilizando-arquitetura-saga-redux-saga/trato-tech-listener/src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.scss';
-
-export default function Button({ children, type, onClick }) {
-  return (
-    <button className={styles.button} type={type} onClick={onClick}>
-      {children}
-    </button>
-  )
-}
