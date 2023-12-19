512308230136d688f1e5a5a8645bd467359212f6
eduqg <eduardoqgomes@gmail.com>
2023-12-19 10:50:25 -0300

    Initial sveltekit spa project

---
+++ svelte-kit-spa/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let name: string = 'Eduardo';
+  let src = 'https://avatars.githubusercontent.com/u/23617963?s=200&v=4';
+  let alt = 'Imagem do Svelte';
+</script>
+
+<h1>Welcome to SvelteKit {name}</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img {src} {alt} height="300" />
